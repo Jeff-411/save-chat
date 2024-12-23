b0c2028ab98e18c65b2d1c7b9fd042491ffa03ea
jeff <johnjeff55@gmail.com>
2024-12-23 13:38:40 -0800

    Update: 'const UI'

--- app-modules/ui/ui.js
+++ app-modules/ui/ui.js
@@ -2,6 +2,10 @@
 
 // prettier-ignore
 export const UI = {
+  CONVERSATION_CONTAINER: 
+  `main .isolate:nth-of-type(2) .pb-\\[144px\\]`,
+  THREAD_CONTAINER: 
+  `main .isolate:nth-of-type(2) .pb-\\[144px\\] > :first-child`,
   QA_PAIRS_CONTAINER: `main .isolate:nth-of-type(2) .pb-\\[144px\\] > :first-child > :first-child`,
   QA_PAIR:
     `main .isolate:nth-of-type(2) .pb-\\[144px\\] > :first-child > :first-child` +
