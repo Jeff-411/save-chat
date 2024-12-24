ab1dade0b997f7b44ad40dfeeefd185360170111
jeff <johnjeff55@gmail.com>
2024-12-24 06:23:15 -0800

    Add: empty 'modules/' folder

--- modules/ui/ui.js
+++
@@ -1,9 +0,0 @@
-// ui.js
-
-const base = `main .isolate:nth-of-type(2)`
-
-// UI constants
-const CONVERSATION = `${base} .pb-\\[144px\\]` // 1st class: mx-auto
-const THREAD = `${CONVERSATION} > :first-child` // 1st class: relative
-const QA_PAIRS = `${THREAD} > :first-child` // no classes
-const QA_PAIR = `${QA_PAIRS} > div:has(> *):not(.pointer-events-none)` // 1st class: pb-lg
