10e9b19261ed5e1bf68fefd82b94d0b926ff9fd7
jeff <johnjeff55@gmail.com>
2024-12-25 13:34:55 -0800

    Update: constants

--- app.js
+++ app.js
@@ -1,17 +1,46 @@
 console.log(`app.js reached...`)
 
-// Scope the selectors
+// LAYOUT
+/**
+ * Scope the selectors. The `base` constant excludes the content of
+ * Perplexity's "Library" section. This handles edge cases in which
+ * the user has been chatting with Perplexity about the Perplexity DOM.
+ */
 const base = `main .isolate:nth-of-type(2)`
-
-// Named constants
-const CONVERSATION = `${base} .pb-\\[144px\\]`
-const THREAD = `${CONVERSATION} > :first-child`
-const QA_PAIRS = `${THREAD} > :first-child`
-const QA_PAIR = `${QA_PAIRS} > div:has(> *):not(.pointer-events-none)`
-const CHAT = `${QA_PAIR} > div > div` // .col-span-8
+/**
+ * The MAIN_CONTAINER div is the only child of its parent and has no classes.
+ */
+const MAIN_CONTAINER = `${base} .pb-\\[144px\\] > :first-child > div`
+const QA_PAIR_CONTAINER = `${MAIN_CONTAINER} > .pb-lg`
+/**
+ * PAIR_LEFT contains the code we want to extract.
+ *
+ * For reference: Its sibling to the right (isolate.col-span-4)
+ * contains the panes used to:
+ *  - Search Images,
+ *  - Search Videos, and
+ *  - Generate Image
+ */
+const PAIR_LEFT = `${QA_PAIR_CONTAINER} > div> .col-span-8`
+/**
+ * In this LAYOUT context, "PAIR_LEFT" seems like a good name for
+ * the path to the .col-span-8 element.
+ *
+ * In a DEVELOPMENT context, however, it makes more sense to refer to
+ * the element we want to extract as a "CHAT".
+ */
+const CHAT = PAIR_LEFT
 const QUESTION = `${CHAT} > :first-child`
-// const ANSWER = `${CHAT} > :last-child`
-const ANSWER = `${CHAT} > :nth-child(3)`
+const ANSWER = `${CHAT} > :last-child`
+
+const selectors = {
+  MAIN_CONTAINER,
+  QA_PAIR_CONTAINER,
+  PAIR_LEFT,
+  CHAT,
+  QUESTION,
+  ANSWER,
+}
 
 function testSelectors(selectors) {
   const results = {}
@@ -38,14 +67,5 @@ function testSelectors(selectors) {
 }
 
 // Usage:
-const selectors = {
-  CONVERSATION,
-  THREAD,
-  QA_PAIRS,
-  QA_PAIR,
-  CHAT,
-  QUESTION,
-  ANSWER,
-}
 
 testSelectors(selectors)
