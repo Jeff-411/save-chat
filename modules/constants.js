736670cf6a4be720dd97fff444c4812eff0bbe0c
Jeff-411 <42661647+Jeff-411@users.noreply.github.com>
2024-12-27 13:16:01 -0800

    Update: selectors (#2)
    * Update: Edit Query Btn selector
    
    * Update: constants.js

--- modules/constants.js
+++ modules/constants.js
@@ -1,7 +1,6 @@
 export const base = `main .isolate:nth-of-type(2)`
-export const MAIN_CONTAINER = `${base} .pb-\\[144px\\] > :first-child > div`
-export const QA_PAIR_CONTAINER = `${MAIN_CONTAINER} > .pb-lg`
+export const EXPORT = `${base} .pb-\\[144px\\] > :first-child > div`
+export const QA_PAIR_CONTAINER = `${EXPORT} > .pb-lg`
 export const PAIR_LEFT = `${QA_PAIR_CONTAINER} > div> .col-span-8`
-export const CHAT = PAIR_LEFT
-export const QUESTION = `${CHAT} > :first-child`
-export const ANSWER = `${CHAT} > :last-child`
+export const QUESTION = `${PAIR_LEFT} > :first-child`
+export const ANSWER = `${PAIR_LEFT} > :last-child`
--- modules/selectors.js
+++ modules/selectors.js
@@ -1,11 +1,4 @@
-import {
-  MAIN_CONTAINER,
-  QA_PAIR_CONTAINER,
-  PAIR_LEFT,
-  CHAT,
-  QUESTION,
-  ANSWER,
-} from './constants.js'
+import { MAIN_CONTAINER, QA_PAIR_CONTAINER, PAIR_LEFT, QUESTION, ANSWER } from './constants.js'
 
 export const selectors = {
   MAIN_CONTAINER,
