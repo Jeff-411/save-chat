f5883ea36d8b270595bf36ce2fe7d340ad2de0b1
jeff <johnjeff55@gmail.com>
2025-01-01 05:55:25 -0800

    Clean-up: remove extractToMd-related code

--- src/app.js
+++ src/app.js
@@ -1,24 +1,3 @@
 // app.js
 
-import { extractToMd1 } from './modules/extractToMd.js'
-
-// Get the HTML content from the current document
-const html = document.documentElement.outerHTML
-
-// Convert to markdown
-const markdown = extractToMd1(html)
-console.log(markdown)
-
-// Optional: Add a button to trigger the conversion
-// document.body.insertAdjacentHTML(
-//   'beforeend',
-//   `
-//   <button
-//     onclick="window.location.href='data:text/markdown;charset=utf-8,${encodeURIComponent(
-//       markdown
-//     )}'"
-//     style="position:fixed; bottom:20px; right:20px;">
-//     Download Markdown
-//   </button>
-// `
-// )
+console.log(`app.js reached...`)
